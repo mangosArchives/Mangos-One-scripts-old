1ef97e09b682416b0a18e4b8a2e093b510c2a305
Xfurry <mail@nomail.com>
2011-10-31 22:38:59 +0200

    [s2327] Readd support for quests 5097 and 5098
    The script names were not deleted so we don't need a SQL update

--- scripts/world/go_scripts.cpp
+++ scripts/world/go_scripts.cpp
@@ -33,6 +33,7 @@ go_sacred_fire_of_life
 go_shrine_of_the_birds
 go_tablet_of_madness
 go_tablet_of_the_seven
+go_andorhal_tower
 go_hand_of_iruxos_crystal
 EndContentData */
 
@@ -355,6 +356,42 @@ bool GOUse_go_blood_filled_orb(Player* pPlayer, GameObject* pGo)
 }
 
 /*######
+## go_andorhal_tower
+######*/
+
+enum
+{
+    QUEST_ALL_ALONG_THE_WATCHTOWERS_ALLIANCE = 5097,
+    QUEST_ALL_ALONG_THE_WATCHTOWERS_HORDE    = 5098,
+    NPC_ANDORHAL_TOWER_1                     = 10902,
+    NPC_ANDORHAL_TOWER_2                     = 10903,
+    NPC_ANDORHAL_TOWER_3                     = 10904,
+    NPC_ANDORHAL_TOWER_4                     = 10905,
+    GO_ANDORHAL_TOWER_1                      = 176094,
+    GO_ANDORHAL_TOWER_2                      = 176095,
+    GO_ANDORHAL_TOWER_3                      = 176096,
+    GO_ANDORHAL_TOWER_4                      = 176097
+};
+
+bool GOUse_go_andorhal_tower(Player* pPlayer, GameObject* pGo)
+{
+    if (pPlayer->GetQuestStatus(QUEST_ALL_ALONG_THE_WATCHTOWERS_ALLIANCE) == QUEST_STATUS_INCOMPLETE || pPlayer->GetQuestStatus(QUEST_ALL_ALONG_THE_WATCHTOWERS_HORDE) == QUEST_STATUS_INCOMPLETE)
+    {
+        uint32 uiKillCredit = 0;
+        switch(pGo->GetEntry())
+        {
+            case GO_ANDORHAL_TOWER_1:   uiKillCredit = NPC_ANDORHAL_TOWER_1;   break;
+            case GO_ANDORHAL_TOWER_2:   uiKillCredit = NPC_ANDORHAL_TOWER_2;   break;
+            case GO_ANDORHAL_TOWER_3:   uiKillCredit = NPC_ANDORHAL_TOWER_3;   break;
+            case GO_ANDORHAL_TOWER_4:   uiKillCredit = NPC_ANDORHAL_TOWER_4;   break;
+        }
+        if (uiKillCredit)
+            pPlayer->KilledMonsterCredit(uiKillCredit);
+    }
+    return true;
+}
+
+/*######
 ## go_hand_of_iruxos_crystal
 ######*/
 
@@ -450,6 +487,11 @@ void AddSC_go_scripts()
     pNewScript = new Script;
     pNewScript->Name = "go_blood_filled_orb";
     pNewScript->pGOUse =          &GOUse_go_blood_filled_orb;
+    pNewScript->RegisterSelf();
+
+    pNewScript = new Script;
+    pNewScript->Name = "go_andorhal_tower";
+    pNewScript->pGOUse =          &GOUse_go_andorhal_tower;
     pNewScript->RegisterSelf();
 
     pNewScript = new Script;
