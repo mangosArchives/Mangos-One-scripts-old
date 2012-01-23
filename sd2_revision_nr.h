ea466047cedb4ee5d65e74ae657f470b52963576
Schmoozerd <schmoozerd@scriptdev2.com>
2012-01-23 22:38:30 +0200

    [s2385] Fix logic bug in Akama script
    Thanks to Reamer for pointing.
    Also move Gossip Item to database
    
    (based on commit e3b896d)

--- sd2_revision_nr.h
+++ sd2_revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __SD2_REVISION_NR_H__
 #define __SD2_REVISION_NR_H__
- #define SD2_REVISION_NR "s2384"
+ #define SD2_REVISION_NR "s2385"
 #endif // __SD2_REVISION_NR_H__
--- sql/scriptdev2_script_full.sql
+++ sql/scriptdev2_script_full.sql
@@ -2647,6 +2647,10 @@ INSERT INTO gossip_texts (entry,content_default,comment) VALUES
 (-3560005,'Strange wizard?','taretha GOSSIP_ITEM_EPOCH1'),
 (-3560006,'We\'ll get you out. Taretha. Don\'t worry. I doubt the wizard would wander too far away.','taretha GOSSIP_ITEM_EPOCH2');
 
+-- -3 564 000 BLACK TEMPLE
+INSERT INTO gossip_texts (entry,content_default,comment) VALUES
+(-3564000,'We are ready to fight alongside you, Akama','akama(shade) GOSSIP_ITEM_START_ENCOUNTER');
+
 
 --
 -- Below just for beautiful view in table, run at own desire
