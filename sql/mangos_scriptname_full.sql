bc75631fc87533f6f40fdd2f6a779f2f37d3aa5b
NeatElves <neatelves@scriptdev2.com>
2012-08-20 00:55:35 +0300

    [s2594] Drop gossip script for npc 14347 - will be handled in DB
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>
    
    (based on commit 5391805)

--- sql/mangos_scriptname_full.sql
+++ sql/mangos_scriptname_full.sql
@@ -749,7 +749,6 @@ UPDATE creature_template SET ScriptName='npc_khadgars_servant' WHERE entry=19685
 UPDATE creature_template SET ScriptName='npc_salsalabim' WHERE entry=18584;
 
 /* SILITHUS */
-UPDATE creature_template SET ScriptName='npc_highlord_demitrian' WHERE entry=14347;
 UPDATE creature_template SET ScriptName='npcs_rutgar_and_frankal' WHERE entry IN (15170,15171);
 UPDATE creature_template SET ScriptName='npc_anachronos_the_ancient' WHERE entry=15381;
 UPDATE gameobject_template SET ScriptName='go_crystalline_tear' WHERE entry=180633;
