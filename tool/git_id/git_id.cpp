c403bc26f9c6c5fd31e8a76309e4e285b554988d
Xfurry <xfurry@scriptdev2.com>
2012-09-04 21:53:58 +0300

    Fix git_id after repo rename

--- tool/git_id/git_id.cpp
+++ tool/git_id/git_id.cpp
@@ -54,8 +54,8 @@
 #define NUM_DATABASES 2
 
 char remotes[NUM_REMOTES][MAX_REMOTE] = {
-    "git@github.com:mangos-one/scriptdev2.git",
-    "git://github.com/mangos-one/scriptdev2.git"                       // used for fetch if present
+    "git@github.com:mangos-one/scripts.git",
+    "git://github.com/mangos-one/scripts.git"                       // used for fetch if present
 };
 
 char remote_branch[MAX_REMOTE] = "master";
